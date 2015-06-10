b97d41d5978af4501d4117558d5c91a7ec282b27
Simon Prickett <simon@crudworks.org>
2015-06-10 19:12:42 -0400

    Made this go faster

--- src/js/allthepokemon.js
+++ src/js/allthepokemon.js
@@ -7,52 +7,23 @@ var allThePokemon = {
 
 	loadPokemon: function() {
 		var pokemonArea = $('#pokemon');
+		var n = 1;
 
-		for (var n = 1; n <= allThePokemon.maxNationalIndex; n++) {
+		for (; n <= allThePokemon.maxNationalIndex; n++) {
 			pokemonArea.append('<div class="onePokemon" id="pokemon' + n + '"><span class="pokemonPlaceholder">' + n + '</span></div>');
 		}
 
-		$.ajax('http://pokeapi.co/api/v1/pokedex/1/')
-			.done(function(data) {
-				allThePokemon.getPokemonDetail(data);
-			})
-			.fail(function() {
-				console.log('Error getting pokemon list :(');
-			}
-		);
-	},
-
-	getPokemonDetail: function(pokemonData) {
-		var pokemon = pokemonData.pokemon;
-
-		for (var n = 0; n < pokemon.length; n ++) {
-			var thisPokemon = pokemon[n];
-			if (Number(thisPokemon.resource_uri.split('/')[3]) <= allThePokemon.maxNationalIndex) {
-				$.ajax('http://pokeapi.co/' + thisPokemon.resource_uri)
-					.done(function(data) {
-						allThePokemon.getSpriteData(data);
-					})
-					.fail(function() {
-						console.log('Error getting pokemon detail :(');
-					}
-				);
-			}
-		}
-	},
-
-	getSpriteData: function(pokemon) {
-		if (pokemon.sprites[0]) {
-			$.ajax('http://pokeapi.co' + pokemon.sprites[0].resource_uri)
-				.done(function(spriteData) {
-					$('#pokemon' + Number(pokemon.national_id)).html('<img class="pokemonImage" src="http://pokeapi.co' + spriteData.image + '" alt="Pokemon Image"/>' + '<br/><span class="pokemonName">(#' + pokemon.national_id + ') ' + pokemon.name + '</span>');
+		for (n = 1; n <= allThePokemon.maxNationalIndex; n++) {
+			$.ajax('http://pokeapi.co/api/v1/pokemon/' + n + '/')
+				.done(function(pokemon) {
+					console.log('looking for #pokemon' + Number(pokemon.national_id));
+					$('#pokemon' + Number(pokemon.national_id)).html('<img class="pokemonImage" src="http://pokeapi.co/media/img/' + Number(pokemon.national_id) + '.png" alt="Pokemon Image"/>' + '<br/><span class="pokemonName">(#' + pokemon.national_id + ') ' + pokemon.name + '</span>');
 				})
 				.fail(function() {
-					console.log('Error getting sprite detail :(');
+					console.log('Error getting pokemon detail :(');
 				}
 			);
-		} else {
-			console.log('No sprite for ' + pokemon.name + ' - ignoring.');
-		} 	
+		}
 	}
 };
 
